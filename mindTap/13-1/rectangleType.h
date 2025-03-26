c9959d7cdedf65d35a03eaa3fbacb14136dae042
YZhang223 <127790675+YZhang223@users.noreply.github.com>
2025-03-26 10:08:00 -0400

    Add files via upload

---
+++ mindTap/13-1/rectangleType.h
@@ -0,0 +1,38 @@
+#ifndef H_rectangleType
+#define H_rectangleType
+  
+#include <iostream> 
+using namespace std;
+
+class rectangleType
+{
+    //overload the << operator
+
+public:
+    void setDimension(double l, double w);
+    double getLength() const;
+    double getWidth() const;
+    double area() const;
+    double perimeter() const;
+
+    //Overload the arithmetic operators +, -, *
+    //perform operations on length and width separately
+    
+
+    //Overload the relational operators ==, !=, <=, <, >=, >
+    //compare the areas of two rectangles and return boolean 
+    
+
+    //Overload the increment and decrement operators
+
+
+    //constructors
+    rectangleType();
+    rectangleType(double l, double w);
+
+protected:
+    double length;
+    double width;
+};
+
+#endif
