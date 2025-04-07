e140ee3cec4d88ec3bc0af6f73d6236e6f3f1874
YZhang223 <127790675+YZhang223@users.noreply.github.com>
2025-04-07 10:49:09 -0400

    Create fibonacci.cpp

---
+++ samplecode/lec15-more exercises on recursion/03-fibonacci/fibonacci.cpp
@@ -0,0 +1,24 @@
+
+// fibonacci.cpp
+
+#include <iostream>
+using namespace std;
+
+// Function prototype
+int fibonacci(int n);
+
+int main() {
+    int n;
+    cout << "Enter a non-negative integer: ";
+    cin >> n;
+
+    int result = fibonacci(n);
+    cout << "Fibonacci number at position " << n << " is " << result << endl;
+
+    return 0;
+}
+
+// TODO: Implement this recursive function
+int fibonacci(int n) {
+    // Your code here
+}
