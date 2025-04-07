30900ab7fefa947127d391e9502136fd5680a78a
YZhang223 <127790675+YZhang223@users.noreply.github.com>
2025-04-07 10:06:41 -0400

    Create sum.cpp

---
+++ samplecode/lec15-recursion exercises/sum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int sum(int, int);
+
+int main()
+{
+  int a, b;
+  cout << "Enter two integers: ";
+  cin >> a >> b;
+
+  cout << sum(a, b) << endl;
+
+}
+
+//precondition: a > 0, b >= a
+//this function returns a + (a+1) + ... + b
+int sum(int a, int b)
+{
+  return 0;
+
+}
