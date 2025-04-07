cfdc52184941a4135ec35f9f256a271c0a6dab8d
YZhang223 <127790675+YZhang223@users.noreply.github.com>
2025-04-07 10:05:26 -0400

    Create power.cpp

---
+++ samplecode/lec15-recursion exercises/power.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int power(int, int);
+
+int main()
+{
+  int a, b;
+  cout << "Enter two integers: ";
+  cin >> a >> b;
+
+  cout << power(a, b) << endl;
+
+}
+
+//precondition: a > 0, b > 0
+//this function returns a^b
+int power(int a, int b)
+{
+  return 0;
+
+}
