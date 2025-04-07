68217de2f45554a02190bcb3eca571aaea3fd867
YZhang223 <127790675+YZhang223@users.noreply.github.com>
2025-04-07 10:47:36 -0400

    Create sum.cpp

---
+++ samplecode/lec15-more exercises on recursion/02-sum-natural/sum.cpp
@@ -0,0 +1,24 @@
+
+// sum.cpp
+
+#include <iostream>
+using namespace std;
+
+// Function prototype
+int sum(int n);
+
+int main() {
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    int result = sum(n);
+    cout << "Sum of first " << n << " natural numbers is " << result << endl;
+
+    return 0;
+}
+
+// TODO: Implement this recursive function
+int sum(int n) {
+    // Your code here
+}
