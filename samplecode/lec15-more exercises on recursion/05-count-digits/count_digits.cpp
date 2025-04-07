4ed163258f88380edb70d4a6af6ad47e59fdd907
YZhang223 <127790675+YZhang223@users.noreply.github.com>
2025-04-07 10:53:21 -0400

    Create count_digits.cpp

---
+++ samplecode/lec15-more exercises on recursion/05-count-digits/count_digits.cpp
@@ -0,0 +1,23 @@
+// count_digits.cpp
+
+#include <iostream>
+using namespace std;
+
+// Function prototype
+int countDigits(int n);
+
+int main() {
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    int result = countDigits(n);
+    cout << "Number of digits in " << n << " is " << result << endl;
+
+    return 0;
+}
+
+// TODO: Implement this recursive function
+int countDigits(int n) {
+    // Your code here
+}
