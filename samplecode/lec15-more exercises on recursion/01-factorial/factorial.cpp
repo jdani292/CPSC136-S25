c65a2ed956ceb591e54cb61cdb01fbeb624f02e5
YZhang223 <127790675+YZhang223@users.noreply.github.com>
2025-04-07 10:45:33 -0400

    Create factorial.cpp

---
+++ samplecode/lec15-more exercises on recursion/01-factorial/factorial.cpp
@@ -0,0 +1,23 @@
+// factorial.cpp
+
+#include <iostream>
+using namespace std;
+
+// Function prototype
+int factorial(int n);
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int result = factorial(n);
+    cout << "Factorial of " << n << " is " << result << endl;
+
+    return 0;
+}
+
+// TODO: Implement this recursive function
+int factorial(int n) {
+    // Your code here
+}
