d5e089d033349ed21212fd864812fdd4c68d96c4
YZhang223 <127790675+YZhang223@users.noreply.github.com>
2025-04-14 09:55:59 -0400

    Add files via upload

---
+++ samplecode/ch17-simpleLinkedList/linkedList.cpp
@@ -0,0 +1,24 @@
+#include "linkedList.h"
+#include <iostream>
+
+using namespace std;
+
+linkedList::linkedList()
+{
+  head = nullptr;
+  tail = nullptr;
+  count = 0;
+}
+
+
+void linkedList::insertAtEnd(int x)
+{
+  
+
+}
+
+void linkedList::print() const
+{
+  
+
+}
